7a987c4665143dec8c535d6ee6b80007a3b49d0b
kapianidze soso <sqafianidze@gmail.com>
2019-10-09 18:42:21 +0400

    initial

--- RateLimiter.Tests/RateLimiterTest.cs
+++ RateLimiter.Tests/RateLimiterTest.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace RateLimiter.Tests
 {
@@ -6,9 +9,92 @@ namespace RateLimiter.Tests
     public class RateLimiterTest
     {
         [Test]
-        public void Example()
+        public void Returns_NotPassed_On_The_Same_Type_Of_Request()
         {
-            Assert.IsTrue(true);
+            var issuers = IssuerFactory.GenerateIssuers();
+            var requestStore = new RequestLimitChecker(issuers);
+            var currentDate = DateTimeOffset.Now;
+
+            var responseOnFirstRequest = requestStore.CheckAndFire(issuers[0].AccessToken, RequestType.A, currentDate, null);
+            Assert.AreEqual(responseOnFirstRequest, LimitCheckerResponse.Passed);
+
+            var responseOnSecondRequest = requestStore.CheckAndFire(issuers[0].AccessToken, RequestType.A, currentDate, null);
+            Assert.AreEqual(responseOnSecondRequest, LimitCheckerResponse.NotPassed);
+        }
+
+        [Test]
+        public void Success_On_Same_Type_Of_Request()
+        {
+            var issuers = IssuerFactory.GenerateIssuers();
+            var requestStore = new RequestLimitChecker(issuers);
+            var currentDate = DateTimeOffset.Now;
+            var calledFirstTime = false;
+            var calledSecondTime = false;
+
+            requestStore.CheckAndFire(issuers[0].AccessToken, RequestType.A, currentDate, () =>
+             {
+                 //api request ....
+                 calledFirstTime = true;
+             });
+            Assert.IsTrue(calledFirstTime);
+
+            requestStore.CheckAndFire(issuers[0].AccessToken, RequestType.B, currentDate, () =>
+            {
+                //api request ....
+                calledSecondTime = true;
+            });
+            Assert.IsTrue(calledSecondTime);
+        }
+
+        [Test]
+        public void Success_With_Same_Type_Two_Times()
+        {
+            var issuers = IssuerFactory.GenerateIssuers();
+
+            var requestStore = new RequestLimitChecker(issuers);
+            var currentDate = DateTimeOffset.Now;
+
+            var firstAnswer = requestStore.CheckAndFire(issuers[0].AccessToken, RequestType.A, currentDate, null);
+            Assert.AreEqual(firstAnswer, LimitCheckerResponse.Passed);
+
+            var secondRequestDate = currentDate.AddMilliseconds(issuers[0].DelaysInMilliseconds + 10);
+            var secondAnswer = requestStore.CheckAndFire(issuers[0].AccessToken, RequestType.A, secondRequestDate, null);
+
+
+            Assert.AreEqual(secondAnswer, LimitCheckerResponse.Passed);
+
+        }
+
+        [Test]
+        public void Success_With_Different_Issuers()
+        {
+            var issuers = IssuerFactory.GenerateIssuers();
+
+            var requestStore = new RequestLimitChecker(issuers);
+            var currentDate = DateTimeOffset.Now;
+
+            var firstAnswer = requestStore.CheckAndFire(issuers[0].AccessToken, RequestType.A, currentDate, null);
+            Assert.AreEqual(firstAnswer, LimitCheckerResponse.Passed);
+
+            var secondAnswer = requestStore.CheckAndFire(issuers[1].AccessToken, RequestType.A, currentDate, null);
+            Assert.AreEqual(secondAnswer, LimitCheckerResponse.Passed);
+
+        }
+
+        [Test]
+        public void Fail_With_Same_Issuer()
+        {
+            var issuers = IssuerFactory.GenerateIssuers();
+
+            var requestStore = new RequestLimitChecker(issuers);
+            var currentDate = DateTimeOffset.Now;
+
+            var firstAnswer = requestStore.CheckAndFire(issuers[0].AccessToken, RequestType.A, currentDate, null);
+            Assert.AreEqual(firstAnswer, LimitCheckerResponse.Passed);
+
+            var secondAnswer = requestStore.CheckAndFire(issuers[0].AccessToken, RequestType.A, currentDate.AddMilliseconds(issuers[0].DelaysInMilliseconds-1), null);
+            Assert.AreEqual(secondAnswer, LimitCheckerResponse.NotPassed);
+
         }
     }
 }
